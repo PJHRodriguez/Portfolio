5ef2f547caaa1e4bbdec77d55d9bf4e9fdbad115
PJHRodriguez <pablo12344445@gmail.com>
2025-05-29 15:42:15 -0600

    download cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,8 @@ export const AboutSection = () => {
                 Contactame
               </a>
               <a
-                href="#contact"
+                href="/public/CV_PJHR.pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Descargar CV
